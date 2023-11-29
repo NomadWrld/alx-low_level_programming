d4913ee19a8377d0a00cdb85744dbc4fe3fa9f4d
NomadWrld <moradamalco@gmail.com>
2023-11-29 14:46:38 -0800

    Fixed

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 /**
  * array_iterator - prints each array elem on a newl
- * *@array* array
- * *@size*: how many element to print
- * *@action* pointer to print in regular or hex
- * *Return* void
+ * @array: array
+ * @size: how many element to print
+ * @action: pointer to print in regular or hex
+ * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 #include "3-calc.h"
 /**
- * *main* Prints the result of simple operations.
- * *@argc* The number of arguments supplied to the program.
- * *@argv* An array of pointers to the arguments.
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
  * Return: Always 0.
  */
